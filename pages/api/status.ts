6a1a2de10a7b7f310577523243c19a4128545b47
z3ro <joaocougia@gmail.com>
2021-01-21 15:10:52 -0300

    Adicionando a rota de status

---
+++ pages/api/status.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res: NextApiResponse) => { 
+    try {
+        res.statusCode = 200; 
+        res.end(JSON.stringify({ status: true, msg: "Plataforma funcionando" }))
+    } catch(errormessage) {
+        res.statusCode = 500;
+        res.end(JSON.stringify({ status: false, msg: "Plataforma em mal funcionamento" }))
+    }
+}
