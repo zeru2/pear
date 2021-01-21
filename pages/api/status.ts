291934d9f8e25deb7d6d27e3cf1205ec24287a57
z3ro <joaocougia@gmail.com>
2021-01-21 17:54:02 -0300

    Add status route

--- pages/api/status.ts
+++ pages/api/status.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => { 
     try {
-        res.statusCode = 200; 
+        res.statusCode = 200;
         res.end(JSON.stringify({ status: true, msg: "Plataforma funcionando" }))
     } catch(errormessage) {
         res.statusCode = 500;
