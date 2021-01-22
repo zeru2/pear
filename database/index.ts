4f542c6c1b086c371df1f94737c48d6744b4c295
z3ro <joaocougia@gmail.com>
2021-01-22 17:55:20 -0300

    Adicionando autentificação ao sistema

--- database/index.ts
+++ database/index.ts
@@ -1,3 +1,3 @@
 import mongoose from 'mongoose';
-mongoose.connect(process.env.MONGO_CONNECT_URI);
+mongoose.connect(process.env.MONGO_CONNECT_URI, { useNewUrlParser: true } );
 module.exports = mongoose
---
+++ pages/api/service/register/[key].ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+const Esatabelecimento =  require('../../../../models/Estabelecimentos')
+
+export default async function register(req: any, res: NextApiResponse) :Promise<void> {
+
+    const {
+        query: { key }
+    } = req;
+
+    if(key === process.env.DEVELOPER_KEY) {
+        try {
+            res.statusCode = 200;
+            
+            const newEstabelecimento = await Esatabelecimento.create(req.body)
+            res.end(JSON.stringify({ newEstabelecimento }))
+    
+        } catch(errormessage) {
+            res.statusCode = 500;
+            res.end(JSON.stringify({ status: false, msg: "Plataforma em mal funcionamento" }))
+        }
+
+        
+    } else {
+        res.end(JSON.stringify({ status: false, msg: "Chave de acesso não existe" }))
+    }
+}
+
+
+// res.end(JSON.stringify({ status: true, msg: "Plataforma funcionando" }))
+// process.env.DEVELOPER_KEY === "KEY"
