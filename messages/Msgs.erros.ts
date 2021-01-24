bb677952ec35370fadf3813dfbb3f3cfc490384e
z3ro <joaocougia@gmail.com>
2021-01-24 14:00:00 -0300

    Adicionando mensagens padronizadas

---
+++ messages/Msgs.erros.ts
@@ -0,0 +1,4 @@
+export const PlataformaEmMalFuncionamento:string = "Plataforma em mal funcionamento";
+export const ChaveDeAcessoErrada:string = "Chave de acesso negada";
+export const PlataformaFuncionando:string = "Plataforma funcionando";
+export const Deletetado:string = "Estabelecimento deletado do bando de dados";
