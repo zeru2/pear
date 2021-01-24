37336de65f7135cad6a81343ad81ff2aec2b46ed
z3ro <joaocougia@gmail.com>
2021-01-24 12:06:53 -0300

    Adicionando padroes a rota de delete; Aplicação padronizada

--- pages/api/service/delete/[key]/[id].ts
+++ pages/api/service/delete/[key]/[id].ts
@@ -9,12 +9,14 @@ export default async function (req, res) {
             const deleteEstabelecimento = await EstabelecimentoSchema.deleteOne({ _id: id });
 
             if(!deleteEstabelecimento) {
-                res.end(JSON.stringify({ status: false, msg: "Id nao existe" }));
+                res.statusCode = 500;
+                return res.end(JSON.stringify({ status: false, msg: "Id nao existe" }));
             }
-
-            res.end(JSON.stringify({ status: false, msg: "Estabelecimento Deletado" }))
+            res.statusCode = 200;
+            res.end(JSON.stringify({ status: true, msg: "Estabelecimento Deletado" }))
         }
     } catch(err) {
+        res.statusCode = 500;
         res.end(JSON.stringify({ status: false, msg: "Plataforma em mal funcionamento" }))
     }
 
