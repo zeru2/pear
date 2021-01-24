e6f0b8c164ad6e3a02d6c9e9d6e6f3900e34f402
z3ro <joaocougia@gmail.com>
2021-01-24 10:53:55 -0300

    Rota de id unico

---
+++ pages/api/service/estabelecimento/[key]/[id].ts
@@ -0,0 +1,25 @@
+const EstabelecimentoSchema = require('../../../../../models/estabelecimentosschema')
+
+export default async function(req, res) {
+    const {
+        query: { id, key }
+    } = req;
+
+    try {
+        if(key === process.env.DEVELOPER_KEY) {
+            const estabelecimeto = await EstabelecimentoSchema.findById(id);
+
+            if(!estabelecimeto) {
+                return res.end(JSON.stringify({ status: false, msg: "Estabelecimento nao existe" }))
+            };
+
+            res.end(JSON.stringify({ status: false, msg: estabelecimeto }))
+
+
+        } else {
+            res.end(JSON.stringify({ status: false, msg: "Chave de acesso não existe" }))
+        }
+    } catch(err) {
+        res.end(JSON.stringify({ status: false, msg: "Plataforma em mal funcionamento" }))
+    }
+}
